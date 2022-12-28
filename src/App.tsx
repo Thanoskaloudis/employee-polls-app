363127e562c862c41c2025a5731acf15fafbd1c6
Thanoskaloudis <8anos.kal@gmail.com>
2022-12-29 01:12:38 +0200

    fix routes and direct after login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Login } from './component/Login/Login';
 import { authSelector } from './features/auth/authSlice';
 import { useAppSelector } from './store/hooks';
-import { AppWrapper } from './component/AppWrapper/AppWrapper';
+import { ResponsiveAppBar } from './component/ResponsiveAppBar/ResponsiveAppBar';
+import { Route, Routes, useNavigate } from 'react-router';
+import { Dashboard, Leaderboard, Poll } from '@mui/icons-material';
 import './App.scss';
 
 function App() {
   const auth = useAppSelector(authSelector);
+  const navigate = useNavigate();
+
+  useEffect(()=> {
+    if(auth.isAuthenticated) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  },[auth.isAuthenticated, navigate])
 
   return (
     <div className="app">
-      {!auth.isAuthenticated ?
-        <Login />
-        : 
-        <AppWrapper />
-      }
+        {auth.isAuthenticated && <ResponsiveAppBar />}
+        <Routes>
+        <Route
+            path="/login"
+            element={<Login />}
+          />
+        <Route
+            path="/leaderboard"
+            element={<Leaderboard />}
+          />
+          <Route
+            path="/"
+            element={<Dashboard />}
+          />
+          <Route
+            path="/answered"
+            element={<Poll />}
+          />
+          <Route
+            path="/polls/:question"
+            element={<Poll />}
+          />
+        </Routes>
     </div>
   );
 }
